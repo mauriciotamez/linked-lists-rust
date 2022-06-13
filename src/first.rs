ae3613b9947b7b6820c7401a446993809b76e5ae
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-06-13 02:44:18 -0500

    added drop trait to our linked list

--- src/first.rs
+++ src/first.rs
@@ -72,3 +72,16 @@ impl List {
         }
     }
 }
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        // while let == "do this thing until this pattern doesn't match anymore"
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+            // boxed_node goes out of scope and gets dropped here;
+            // but its Node's `next` field has been set to Link::Empty
+            // so no unbounded recursion occurs.
+        }
+    }
+}
